d187cb0322e5f361feec35ae4843647691601f97
OumaESS <57940173+OumaESS@users.noreply.github.com>
2020-04-12 20:37:09 +0100

    lise le code propre

--- Montre challenge/script.js
+++ Montre challenge/script.js
@@ -8,9 +8,9 @@ function demarrerLaMontre() {
     let min = heurActuel.getMinutes();
     let sec = heurActuel.getSeconds();
 
-    let secDeg = sec * 6; // sec * 6 --- pour terminer la tour de 360 deg dans 60 step il doit passer 6 Deg pour chaque step
-    let minDeg = (min * 6) + (0.1 * sec); //==> 6 / 60 = 0.1 --- chaque step de laguille sec, laguille min il doit passer 0.1 deg
-    let hrDeg = (hr * 30) + (0.5 * min); //==> 30 / 60 = 0.5 --- chaque step de laguille min , laguille heur doit passer 0.5 deg 
+    let secDeg = sec * 6; 
+    let minDeg = (min * 6) + (0.1 * sec); 
+    let hrDeg = (hr * 30) + (0.5 * min); 
 
     AIGUILLESEC.style.transform = 'rotate(' + secDeg + 'deg)';
     AIGUILLEMIN.style.transform = 'rotate(' + minDeg + 'deg)';
