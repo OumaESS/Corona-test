1dc54829e3bc50834af84a1e9ca5ed4af334e202
OumaESS <57940173+OumaESS@users.noreply.github.com>
2020-05-01 13:51:04 +0000

    add todo first version

---
+++ Todolist/todo.js
@@ -0,0 +1,168 @@
+//ADD new To do 
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const popup = document.querySelector('.popup');
+const gPop = document.querySelector('.popup-wrapper');
+const btn = document.querySelector('.btn');
+const search = document.querySelector('.search input');
+gPop.style.display = "none";
+
+
+/***************reusable function********************/
+
+/* Function pour l'alert et le popup qui va etre afficher (time control)*/
+function start(duree) {
+   var o = document.getElementById("sp");
+   if (duree > 0) {
+      o.innerHTML = duree;
+      gPop.style.display = "block";
+      setTimeout("start(" + duree + " -1)", 1000);
+   } else {
+      alert("enter a valid to do");
+      o.innerHTML = "Au revoir";
+      gPop.style.display = "none";
+      popup.style.visibility = "hidden";
+
+   }
+};
+
+
+/* Function Creation dynamique du POPUP */
+
+function create() {
+   const div = document.createElement('div');
+   div.classList.add('popup-close');
+   div.setAttribute('id', 'closing');
+   const text = document.createTextNode('X');
+   div.appendChild(text);
+   popup.append(div);
+   const div2 = document.createElement('div');
+   div2.classList.add('popup-content');
+   const html = `
+   <span id="sp">1</span>
+   <h2>Fill the Input</h2>
+   <p>Don't forget</p>
+   <a href="#">Return</a>`;
+   div2.innerHTML = html;
+   popup.append(div2);
+
+}
+
+/* Function generation dynamique des TODOS */
+
+const generateTemp = todo => {
+   const html = `
+   <li class="list-group-item d-flex justify-content-between align-items-center">
+             <span>${todo}</span>
+             <i class="fas fa-trash delete"></i>
+            </li>
+   `;
+   list.innerHTML += html;
+};
+
+
+/* function pour controller l'evenement et pour ne pas etre repeté à chaque clique */
+function onetime(node, type, callback) {
+
+   node.addEventListener(type, function (e) {
+
+      e.target.removeEventListener(e.type, arguments.callee);
+
+      return callback(e);
+   });
+}
+
+onetime(gPop, 'click', handler);
+
+function handler(e) {
+
+   if (e.target.id = 'closing') {
+
+      gPop.style.display = "none";
+   }
+}
+
+/***************Fin reusable function********************/
+
+
+
+
+/************* Adding TO DO**************/
+
+//Eventlistner Add TODOS
+btn.addEventListener('click', e => {
+   e.preventDefault();
+   let nbr=3;
+   if (document.querySelector('.popup-content') == null) {
+      create();
+   }
+   if (addForm.add.value == "") {
+      start(nbr);
+      popup.style.visibility = "visible";
+   } else {
+       addf = generateTemp(addForm.add.value);
+      localStorage.getItem(addForm.add.value);
+   }
+
+});
+
+/************* Fin Adding TO DO**************/
+
+
+
+/*************Deleting  TO DO**************/
+
+
+list.addEventListener('click', e => {
+const i=document.querySelector("i");
+const li=document.querySelector("li");
+   if (i.classList.contains('delete')) {
+
+      li.remove();
+   }
+
+});
+
+
+
+/************* Fin Deleting  TO DO**************/
+
+
+
+
+/************************************* SEARCH ITEM********************************************/
+//filtering Todos :
+
+//we will apply a class to the Todos that dont match and the that class will
+
+// have keyup event 
+
+
+
+const retrieve = (term) => {
+
+   //function pour faire un filtre i
+};
+
+
+//evenement de recherche des mots clés 
+search.addEventListener('keyup', () => {
+
+   var filter, li, a, i, txtValue;
+   filter = search.value.toUpperCase();
+   li = list.getElementsByTagName("li");
+   for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("span")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+         li[i].style.display = "";
+      } else {
+         li[i].style.setProperty('display', 'none', 'important');
+
+      }
+   }
+
+
+})
+
+/*************************************Fin SEARCH ITEM********************************************/
